6fa7f411c345516f017cac3b51338f6930ddabe0
oven425 <oven425@yahoo.com.tw>
2022-05-09 19:56:33 +0800

    加入專案檔案。

---
+++ QSoft.DevCon/DevCon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSoft.DevCon
+{
+    public class DeviceControl
+    {
+        public void Where(Expression<Func<DeviceInfo, bool>> func)
+        {
+
+        }
+    }
+
+    public class DeviceInfo
+    {
+        public string HardwareID { set; get; }
+    }
+}
