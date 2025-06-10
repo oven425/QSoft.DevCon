1fd530b51784fefd9e2ea134dc10c252f31b9863
oven425 <oven425@yahoo.com.tw>
2025-06-10 21:07:50 +0800

    update
    1. fix DevicesFromInterface fail at .net8
    2. finding audio capture device path

--- ConsoleApp_NET8/Program.cs
+++ ConsoleApp_NET8/Program.cs
@@ -1,6 +1,15 @@
 ﻿using QSoft.DevCon;
 
 
+var cameras = DevConExtension.KSCATEGORY_AUDIO.DevicesFromInterface()
+                .Select(x => new
+                {
+                    devicepath = x.DevicePath(),
+                    friendname = x.As().GetFriendName(),
+                    classname = x.As().GetClass(),
+                    panel = x.As().Panel(),
+                }).ToList();
+
 var aaa = "Camera".Devices().Select(x => new
 {
     id = x.DeviceInstanceId(),
--- QSoft.DevCon/DevCon_LibraryImport.cs
+++ QSoft.DevCon/DevCon_LibraryImport.cs
@@ -91,7 +91,7 @@ namespace QSoft.DevCon
         [return: MarshalAs(UnmanagedType.Bool)]
         static internal partial bool SetupDiGetDeviceInterfaceDetail(IntPtr DeviceInfoSet, SP_DEVICE_INTERFACE_DATA DeviceInterfaceData, IntPtr DeviceInterfaceDetailData, uint DeviceInterfaceDetailDataSize, out uint RequiredSize, ref SP_DEVINFO_DATA DeviceInfoData);
 
-        [LibraryImport("setupapi.dll", EntryPoint = "SetupDiEnumDeviceInterfacesW", SetLastError = true)]
+        [LibraryImport("setupapi.dll", EntryPoint = "SetupDiEnumDeviceInterfaces", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static internal partial bool SetupDiEnumDeviceInterfaces(IntPtr DeviceInfoSet, IntPtr DeviceInfoData, Guid InterfaceClassGuid, uint MemberIndex, out SP_DEVICE_INTERFACE_DATA DeviceInterfaceData);
 
