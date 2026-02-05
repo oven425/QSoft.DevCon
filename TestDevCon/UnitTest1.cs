51cabbfae3bf0711d2670b32789f37a5fc77d1e4
oven425 <oven425@yahoo.com.tw>
2026-02-05 16:38:43 +0800

    update
    1. test

--- TestDevCon/UnitTest1.cs
+++ TestDevCon/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Management.Infrastructure;
+using QSoft.DevCon;
 
 namespace TestDevCon
 {
@@ -7,22 +8,31 @@ namespace TestDevCon
         [Fact]
         public void Test1()
         {
-            using CimSession session = CimSession.Create(null);
-            // 查詢 Windows PnP 實體，篩選與相機相關的服務或類別
-            string query = "SELECT * FROM Win32_PnPEntity WHERE PNPClass = 'Camera' OR Service = 'usbvideo'";
-            var instances = session.QueryInstances(@"root\cimv2", "WQL", query);
+            //using CimSession session = CimSession.Create(null);
+            //// 查詢 Windows PnP 實體，篩選與相機相關的服務或類別
+            //string query = "SELECT * FROM Win32_PnPEntity WHERE PNPClass = 'Camera' OR Service = 'usbvideo'";
+            //var instances = session.QueryInstances(@"root\cimv2", "WQL", query);
 
-            foreach (var instance in instances)
+            //foreach (var instance in instances)
+            //{
+            //    Console.WriteLine($"[MI 來源]");
+            //    Console.WriteLine($"名稱: {instance.CimInstanceProperties["Name"].Value}");
+            //    Console.WriteLine($"DeviceID: {instance.CimInstanceProperties["PNPDeviceID"].Value}");
+            //    System.Diagnostics.Trace.WriteLine($"{instance.CimInstanceProperties["InstallDate"].Value}");
+            //    // HardwareID 通常是字串陣列
+            //    var hwIds = (string[])instance.CimInstanceProperties["HardwareID"].Value;
+            //    Console.WriteLine($"HardwareID: {string.Join(", ", hwIds)}");
+            //    Console.WriteLine();
+            //}
+            var dds = Guid.Empty.Devices().Select(x => new
             {
-                Console.WriteLine($"[MI 來源]");
-                Console.WriteLine($"名稱: {instance.CimInstanceProperties["Name"].Value}");
-                Console.WriteLine($"DeviceID: {instance.CimInstanceProperties["PNPDeviceID"].Value}");
-                System.Diagnostics.Trace.WriteLine($"{instance.CimInstanceProperties["InstallDate"].Value}");
-                // HardwareID 通常是字串陣列
-                var hwIds = (string[])instance.CimInstanceProperties["HardwareID"].Value;
-                Console.WriteLine($"HardwareID: {string.Join(", ", hwIds)}");
-                Console.WriteLine();
+                name = x.GetFriendName()??x.DeviceDesc()
+            });
+            foreach (var d in dds)
+            {
+                Assert.Fail($"name:{d.name}");
             }
+            
         }
     }
 }
