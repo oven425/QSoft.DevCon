60ade133272182349f9ae67c05200f267bad9974
oven425 <oven425@yahoo.com.tw>
2026-03-12 08:47:23 +0800

    update
    1. study get battery info flow

--- ConsoleApp_NET8/DevCon_Battery.cs
+++ ConsoleApp_NET8/DevCon_Battery.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,32 @@ namespace ConsoleApp_NET8
     {
         public static void GetBatteryInfo(this SafeFileHandle src)
         {
-            uint queryTagWait = 0; // 0 表示立即回傳，不等待
-            uint batteryTag = 0;
-            uint bytesReturned = 0;
+            BATTERY_QUERY_INFORMATION info = new();
+            info.InformationLevel = BatteryInformation;
+            Span<byte> span_in = stackalloc byte[sizeof(uint)];
+            var span_out = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref info.BatteryTag, 1));
 
-            bool success = DeviceIoControl(
-                src,
-                IOCTL_BATTERY_QUERY_TAG,
-                ref queryTagWait,
-                sizeof(uint),
-                ref batteryTag,
-                sizeof(uint),
-                out bytesReturned,
-                IntPtr.Zero
-            );
-
-            BATTERY_QUERY_INFORMATION info = new BATTERY_QUERY_INFORMATION();
-            info.InformationLevel = 0;
-            
-            uint dwWait = 0;
-            var span_in = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref dwWait, 1));
-            //var span_out = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref info.BatteryTag, 1));
-            Span<byte> span_out = stackalloc byte[4];
             var hr = DeviceIoControl(src, IOCTL_BATTERY_QUERY_TAG, span_in, (uint)span_in.Length, span_out, (uint)span_out.Length, out var reqsz, IntPtr.Zero);
+
+
+            info.InformationLevel = BatteryManufactureName;
+            span_in = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref info, 1));
+            
+            hr = DeviceIoControl(src, IOCTL_BATTERY_QUERY_INFORMATION, span_in, (uint)span_in.Length, [], 0, out reqsz, IntPtr.Zero);
+            span_out = stackalloc byte[(int)reqsz*4];
+            hr = DeviceIoControl(src, IOCTL_BATTERY_QUERY_INFORMATION, span_in, (uint)span_in.Length, span_out, (uint)span_out.Length, out reqsz, IntPtr.Zero);
+            var err = Marshal.GetLastWin32Error();
+
+            BATTERY_WAIT_STATUS batter_wait_status = new()
+            {
+                BatteryTag = info.BatteryTag
+            };
+            span_in = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref batter_wait_status, 1));
+            BATTERY_STATUS batter_status = new();
+            span_out = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref batter_status, 1));
+            hr = DeviceIoControl(src, IOCTL_BATTERY_QUERY_STATUS, span_in, (uint)span_in.Length, span_out, (uint)span_out.Length, out reqsz, IntPtr.Zero);
+
+            
         }
 
         struct BATTERY_QUERY_INFORMATION
@@ -43,7 +47,35 @@ namespace ConsoleApp_NET8
             public int InformationLevel;
             public int AtRate;
         }
-        const uint IOCTL_BATTERY_QUERY_TAG = 0x290000;
+
+        public struct BATTERY_WAIT_STATUS 
+        {
+            public uint BatteryTag;
+            public uint Timeout;
+            public uint PowerState;
+            public uint LowCapacity;
+            public uint HighCapacity;
+        }
+
+        public enum PowerState : uint
+        {
+            BATTERY_POWER_ON_LINE = 0x00000001,
+            BATTERY_DISCHARGING = 0x00000002,
+            BATTERY_CHARGING = 0x00000004,
+            BATTERY_CRITICAL = 0x00000008
+        }
+
+        public struct BATTERY_STATUS
+        {
+            public PowerState PowerState;
+            public uint Capacity;
+            public uint Voltage;
+            public int Rate;
+        }
+
+        const uint IOCTL_BATTERY_QUERY_TAG = 0x00294040;
+        const uint IOCTL_BATTERY_QUERY_STATUS = 0x0029404c;
+        const uint IOCTL_BATTERY_QUERY_INFORMATION = 0x00294044;
         const int BatteryInformation = 0;
         const int BatteryGranularityInformation = 1;
         const int BatteryTemperature = 2;
@@ -57,7 +89,7 @@ namespace ConsoleApp_NET8
 
         [LibraryImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static partial bool DeviceIoControl(SafeFileHandle hDevice, uint dwIoControlCode, ReadOnlySpan<byte> lpInBuffer, uint nInBufferSize, Span<byte> lpOutBuffer, uint nOutBufferSize, out uint lpBytesReturned, IntPtr lpOverlapped);
+        internal static partial bool DeviceIoControl(SafeFileHandle hDevice, uint dwIoControlCode, ReadOnlySpan<byte> lpInBuffer, uint nInBufferSize, ReadOnlySpan<byte> lpOutBuffer, uint nOutBufferSize, out uint lpBytesReturned, IntPtr lpOverlapped);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool DeviceIoControl(
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,181 @@
+﻿// ConsoleApplication2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <windows.h>
+#include <setupapi.h>
+#include <batclass.h>
+#include <devguid.h>
+#include <powrprof.h>
+
+#pragma comment(lib, "powrprof.lib")
+#pragma comment(lib, "setupapi.lib")
+
+DWORD GetBatteryState()
+{
+#define GBS_HASBATTERY 0x1
+#define GBS_ONBATTERY  0x2
+    // Returned value includes GBS_HASBATTERY if the system has a 
+    // non-UPS battery, and GBS_ONBATTERY if the system is running on 
+    // a battery.
+    //
+    // dwResult & GBS_ONBATTERY means we have not yet found AC power.
+    // dwResult & GBS_HASBATTERY means we have found a non-UPS battery.
+
+    DWORD dwResult = GBS_ONBATTERY;
+
+    // IOCTL_BATTERY_QUERY_INFORMATION,
+    // enumerate the batteries and ask each one for information.
+
+    HDEVINFO hdev =
+        SetupDiGetClassDevs(&GUID_DEVCLASS_BATTERY,
+            0,
+            0,
+            DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+    if (INVALID_HANDLE_VALUE != hdev)
+    {
+        // Limit search to 100 batteries max
+        for (int idev = 0; idev < 100; idev++)
+        {
+            SP_DEVICE_INTERFACE_DATA did = { 0 };
+            did.cbSize = sizeof(did);
+
+            if (SetupDiEnumDeviceInterfaces(hdev,
+                0,
+                &GUID_DEVCLASS_BATTERY,
+                idev,
+                &did))
+            {
+                DWORD cbRequired = 0;
+
+                SetupDiGetDeviceInterfaceDetail(hdev,
+                    &did,
+                    0,
+                    0,
+                    &cbRequired,
+                    0);
+                if (ERROR_INSUFFICIENT_BUFFER == GetLastError())
+                {
+                    PSP_DEVICE_INTERFACE_DETAIL_DATA pdidd =
+                        (PSP_DEVICE_INTERFACE_DETAIL_DATA)LocalAlloc(LPTR,
+                            cbRequired);
+                    if (pdidd)
+                    {
+                        pdidd->cbSize = sizeof(*pdidd);
+                        if (SetupDiGetDeviceInterfaceDetail(hdev,
+                            &did,
+                            pdidd,
+                            cbRequired,
+                            &cbRequired,
+                            0))
+                        {
+                            // Enumerated a battery.  Ask it for information.
+                            HANDLE hBattery =
+                                CreateFile(pdidd->DevicePath,
+                                    GENERIC_READ | GENERIC_WRITE,
+                                    FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                    NULL,
+                                    OPEN_EXISTING,
+                                    FILE_ATTRIBUTE_NORMAL,
+                                    NULL);
+                            if (INVALID_HANDLE_VALUE != hBattery)
+                            {
+                                // Ask the battery for its tag.
+                                BATTERY_QUERY_INFORMATION bqi = { 0 };
+
+                                DWORD dwWait = 0;
+                                DWORD dwOut;
+                                auto ttga = IOCTL_BATTERY_QUERY_INFORMATION;
+                                if (DeviceIoControl(hBattery,
+                                    IOCTL_BATTERY_QUERY_TAG,
+                                    &dwWait,
+                                    sizeof(dwWait),
+                                    &bqi.BatteryTag,
+                                    sizeof(bqi.BatteryTag),
+                                    &dwOut,
+                                    NULL)
+                                    && bqi.BatteryTag)
+                                {
+                                    // With the tag, you can query the battery info.
+                                    BATTERY_INFORMATION bi = { 0 };
+                                    bqi.InformationLevel = BatteryInformation;
+
+                                    if (DeviceIoControl(hBattery,
+                                        IOCTL_BATTERY_QUERY_INFORMATION,
+                                        &bqi,
+                                        sizeof(bqi),
+                                        &bi,
+                                        sizeof(bi),
+                                        &dwOut,
+                                        NULL))
+                                    {
+                                        // Only non-UPS system batteries count
+                                        if (bi.Capabilities & BATTERY_SYSTEM_BATTERY)
+                                        {
+                                            if (!(bi.Capabilities & BATTERY_IS_SHORT_TERM))
+                                            {
+                                                dwResult |= GBS_HASBATTERY;
+                                            }
+
+                                            // Query the battery status.
+                                            BATTERY_WAIT_STATUS bws = { 0 };
+                                            bws.BatteryTag = bqi.BatteryTag;
+
+                                            BATTERY_STATUS bs;
+                                            if (DeviceIoControl(hBattery,
+                                                IOCTL_BATTERY_QUERY_STATUS,
+                                                &bws,
+                                                sizeof(bws),
+                                                &bs,
+                                                sizeof(bs),
+                                                &dwOut,
+                                                NULL))
+                                            {
+                                                if (bs.PowerState & BATTERY_POWER_ON_LINE)
+                                                {
+                                                    dwResult &= ~GBS_ONBATTERY;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                CloseHandle(hBattery);
+                            }
+                        }
+                        LocalFree(pdidd);
+                    }
+                }
+            }
+            else  if (ERROR_NO_MORE_ITEMS == GetLastError())
+            {
+                break;  // Enumeration failed - perhaps we're out of items
+            }
+        }
+        SetupDiDestroyDeviceInfoList(hdev);
+    }
+
+    //  Final cleanup:  If we didn't find a battery, then presume that we
+    //  are on AC power.
+
+    if (!(dwResult & GBS_HASBATTERY))
+        dwResult &= ~GBS_ONBATTERY;
+
+    return dwResult;
+}
+
+int main()
+{
+    GetBatteryState();
+    std::cout << "Hello World!\n";
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
